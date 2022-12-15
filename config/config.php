e849ace8b133eaa9372e33f45795d0dd4d637352
FLopDel <99647790+FLopDel@users.noreply.github.com>
2022-12-15 17:52:04 +0100

    config

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+define('SERVIDOR','localhost');
+define('USUARIO','admin');
+define('PASS','admin123');
+define('BASE_DATOS','blog');
+
+
+define("base_url", "http://localhost/proyecto/");
+
+
+define("controller_default", "controllers\CategoriaController");
+define("action_default", "mostrarTodos");
